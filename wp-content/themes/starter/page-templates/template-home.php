b4cff514d9d8fb291112726bf00536013bde1cca
Garrett Cullen <garrettwcullen@gmail.com>
2018-12-21 09:42:16 -0800

    push

--- wp-content/themes/starter/page-templates/template-home.php
+++ wp-content/themes/starter/page-templates/template-home.php
@@ -2,12 +2,11 @@
 	
 	/* Template Name: Home */
 	
-	get_header(); ?>
+	get_header();
 
 
+	get_template_part( 'page-templates/homepage_template_parts/section', '1' );
+	
+	
+	get_footer(); ?>
 
-<?php get_template_part( 'page-templates/homepage_template_parts/section', '1' );?>
-
-
-
-<?php get_footer(); ?>
