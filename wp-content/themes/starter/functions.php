6ea0e986269b645eb0cdf6f38b0bcde8f8e3d750
Garrett Cullen <Garth619@users.noreply.github.com>
2020-03-04 09:11:04 -0800

    push

--- wp-content/themes/starter/functions.php
+++ wp-content/themes/starter/functions.php
@@ -393,5 +393,32 @@ function wpbeginner_numeric_posts_nav() {
 }
 
 
+// Responsive Content Images */
+function ilawyer_content_image_sizes_attr( $sizes, $size ) {
+	$width = $size[0];
+​
+	840 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 62vw, 840px';
+​
+	if ( 'page' === get_post_type() ) {
+		840 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
+	} else {
+		840 > $width && 600 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 61vw, (max-width: 1362px) 45vw, 600px';
+		600 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
+	}
+​
+	return$sizes;
+}
+add_filter( 'wp_calculate_image_sizes', 'ilawyer_content_image_sizes_attr', 10 , 2 );
+​
+// Responsive Featured Images 
+function ilawyer_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
+	if ( 'post-thumbnail' === $size ) {
+		is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
+		! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
+	}
+	return$attr;
+}
+add_filter( 'wp_get_attachment_image_attributes', 'ilawyer_post_thumbnail_sizes_attr', 10 , 3 );
+
 
 
